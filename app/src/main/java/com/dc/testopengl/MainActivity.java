ef45b648a2e269478635188d7b1a1c4c54dcbb9d
duancong <congduan@yeah.net>
2018-03-25 15:35:18 +0800

    full screen

--- app/src/main/java/com/dc/testopengl/MainActivity.java
+++ app/src/main/java/com/dc/testopengl/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 全屏显示
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         initGLSurfaceView();
     }
