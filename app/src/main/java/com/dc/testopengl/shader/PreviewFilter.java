7975f698a71da9b6dce1ce0c234a28b27b425be6
duancong <congduan@yeah.net>
2018-07-21 14:44:55 +0800

    去掉黑白效果

--- app/src/main/java/com/dc/testopengl/shader/PreviewFilter.java
+++ app/src/main/java/com/dc/testopengl/shader/PreviewFilter.java
@@ -40,10 +40,8 @@ public class PreviewFilter extends Filter {
             "{\n" +
             //获取此纹理（预览图像）对应坐标的颜色值
             "  vec4 vCameraColor = texture2D(uTextureSampler, vTextureCoord);\n" +
-            //求此颜色的灰度值
-            "  float fGrayColor = (0.3*vCameraColor.r + 0.59*vCameraColor.g + 0.11*vCameraColor.b);\n" +
             //将此灰度值作为输出颜色的RGB值，这样就会变成黑白滤镜
-            "  gl_FragColor = vec4(fGrayColor, fGrayColor, fGrayColor, 1.0);\n" +
+            "  gl_FragColor = vCameraColor;\n" +
             "}\n";
 
     //每行前两个值为顶点坐标，后两个为纹理坐标
